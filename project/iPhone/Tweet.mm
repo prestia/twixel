defbdbf7aafaee152021b22e7b9e36a41013876f
Anthony Prestia <prestia@gmail.com>
2014-02-20 13:18:57 -0500

    Fully functional URL support.

--- project/iPhone/Tweet.mm
+++ project/iPhone/Tweet.mm
@@ -4,14 +4,18 @@
 namespace twixel {
     bool Tweet(const char* message, const char* url){
         NSString* textString = [[NSString alloc] initWithUTF8String:message];
-        NSString* urlString = [[NSString alloc] initWithUTF8String:message];
+        NSString* urlString = [[NSString alloc] initWithUTF8String:url];
+        //NSURL* urlString = [[NSURL alloc] initWithString:url];
  
         //if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
             SLComposeViewController *tweetSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
             [tweetSheet setInitialText:textString];
             [tweetSheet addURL:[NSURL URLWithString:urlString]];
             [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentViewController:tweetSheet animated:YES completion:nil];
+            return true;
+        //} else {
+        //    return false;
         //}
-        return true;
+        
     }
 }
